4e4dd22bc03bfd0f7eaeb6f659dacde5520e45b4
malangarita <miguel.angel.langarita@gmail.com>
2015-02-10 13:23:02 +0100

    EvaluacionesDAO (Listado,Crear, Editar, Eliminar) y refactor de EvaluacionesVO

--- src/main/java/es/open4job/sigad/DAO/EvaluacionesDAO.java
+++ src/main/java/es/open4job/sigad/DAO/EvaluacionesDAO.java
@@ -1,6 +1,128 @@
 package es.open4job.sigad.DAO;
 
-public class EvaluacionesDAO {
-	
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import es.open4job.sigad.VO.EvaluacionesVO;
+/*
+@ManagedBean
+@SessionScoped*/
+public class EvaluacionesDAO {
+
+	private DataSource ds;
+
+	public EvaluacionesDAO() {
+
+		try {
+			Context ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:/comp/env/jdbc/oracle");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	// Listar todas las evaluaciones
+	public ArrayList<EvaluacionesVO> getallEvaluaciones() {
+		ArrayList<EvaluacionesVO> listaEvaluaciones = new ArrayList<EvaluacionesVO>();
+		try {
+			Connection conexion = ds.getConnection();
+			Statement stm = conexion.createStatement();
+			ResultSet rs = stm.executeQuery("select * from evaluacion");
+			while (rs.next()) {
+				EvaluacionesVO evaluacion = new EvaluacionesVO();
+				evaluacion.setId_evaluacion(rs.getInt("id"));
+				evaluacion.setId_ensenanza(rs.getInt("id_enseñanza"));
+				evaluacion.setId_curso(rs.getInt("id_curso"));
+				evaluacion.setEvaluacion(rs.getInt("evaluacion"));
+				evaluacion.setFecha_inicio(rs.getDate("fecha_inicio"));
+				evaluacion.setFecha_sesion(rs.getDate("fecha_sesion"));
+				evaluacion.setFecha_fin(rs.getDate("fecha_final"));
+				evaluacion
+						.setFecha_publicacion(rs.getDate("fecha_publicacion"));
+
+				listaEvaluaciones.add(evaluacion);
+			}
+		} catch (Exception e) {
+			Logger.getLogger(getClass().getName()).log(
+					Level.SEVERE,
+					"Error en EvaluacionesDAO.getallEvaluaciones:"
+							+ e.getMessage());
+		}
+		return listaEvaluaciones;
+	}
+
+	// Crear nueva evaluacion
+	public void InsertarEvaluacionesVO() {
+		try {
+			EvaluacionesVO evaluacion = new EvaluacionesVO();
+			Connection conexion = ds.getConnection();
+			PreparedStatement pstm = conexion
+					.prepareStatement("Insert into evaluacion Values (?,?,?,?,?,?,?,?)");
+			pstm.setInt(1, evaluacion.getId_evaluacion());
+			pstm.setInt(2, evaluacion.getId_ensenanza());
+			pstm.setInt(3, evaluacion.getId_curso());
+			pstm.setInt(4, evaluacion.getEvaluacion());
+			pstm.setDate(5, evaluacion.getFecha_inicio());
+			pstm.setDate(6, evaluacion.getFecha_sesion());
+			pstm.setDate(7, evaluacion.getFecha_fin());
+			pstm.setDate(8, evaluacion.getFecha_publicacion());
+			pstm.execute();
+		} catch (Exception e) {
+
+			Logger.getLogger(getClass().getName()).log(Level.SEVERE,
+					e.getMessage());
+		}
+	}
+
+	// Editar una evaluacion
+	public void EditarEvaluacionesVO() {
+		try {
+			Connection conexion = ds.getConnection();
+			PreparedStatement pstm = conexion
+					.prepareStatement("Update evaluacion set id = ? , id_enseñanza = ? ,"
+							+ " id_curso = ?, evaluacion = ?, fecha_inicio = ?, "
+							+ "fecha_sesion = ?, fecha_final = ? , fecha_publicacion = ? Where id= ?");
+			/*pstm.setInt(1, );
+			pstm.setInt(1, );
+			pstm.setInt(1, );
+			pstm.setInt(2, );
+			pstm.setDate(1, );
+			pstm.setDate(1, );
+			pstm.setDate(1, );
+			pstm.setDate(1, );
+			*/
+			pstm.executeUpdate();
+		} catch (Exception e) {
+	
+			Logger.getLogger(getClass().getName()).log(Level.SEVERE,
+					e.getMessage());
+		}
+	}
+
+	// Eliminar una evaluacion 
+	public void EliminarEvaluacionesVO(){
+		Connection conexion;
+		try {
+			conexion = ds.getConnection();
+			PreparedStatement pstm = conexion
+					.prepareStatement("DELETE FROM evaluacion WHERE id=?");
+			//pstm.setInt(1,);
+			pstm.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
--- src/main/java/es/open4job/sigad/VO/EvaluacionesVO.java
+++ src/main/java/es/open4job/sigad/VO/EvaluacionesVO.java
@@ -4,29 +4,51 @@ import java.sql.Date;
 
 public class EvaluacionesVO {
 	private int id_evaluacion;
+	private int id_ensenanza;
+	private int id_curso;
+	private int evaluacion;
 	private Date fecha_inicio;
 	private Date fecha_fin;
-	private Date fecha_evaluacion;
+	private Date fecha_sesion;
 	private Date fecha_publicacion;
 	
 	public EvaluacionesVO() {
-		// TODO Auto-generated constructor stub
+		
 	}
 	
-	public EvaluacionesVO(int id, Date fecha_inicio, Date fecha_fin, Date fecha_evaluacion, Date fecha_publicacion) {
-		this.id_evaluacion = id;
+	public EvaluacionesVO(int id_evaluacion, int id_ensenanza, int id_curso,int evaluacion,Date fecha_inicio, Date fecha_fin, Date fecha_evaluacion, Date fecha_publicacion) {
+		this.id_evaluacion = id_evaluacion;
+		this.id_ensenanza = id_ensenanza;
+		this.id_curso = id_curso;
+		this.evaluacion=evaluacion;
 		this.fecha_inicio = fecha_inicio;
 		this.fecha_fin = fecha_fin;
-		this.fecha_evaluacion = fecha_evaluacion;
+		this.fecha_sesion = fecha_evaluacion;
 		this.fecha_publicacion = fecha_publicacion;
 	}
 
-	public int getId() {
+	public int getId_evaluacion() {
 		return id_evaluacion;
 	}
 
-	public void setId(int id) {
-		this.id_evaluacion = id;
+	public void setId_evaluacion(int id_evaluacion) {
+		this.id_evaluacion = id_evaluacion;
+	}
+
+	public int getId_ensenanza() {
+		return id_ensenanza;
+	}
+
+	public void setId_ensenanza(int id_ensenanza) {
+		this.id_ensenanza = id_ensenanza;
+	}
+
+	public int getId_curso() {
+		return id_curso;
+	}
+
+	public void setId_curso(int id_curso) {
+		this.id_curso = id_curso;
 	}
 
 	public Date getFecha_inicio() {
@@ -45,21 +67,31 @@ public class EvaluacionesVO {
 		this.fecha_fin = fecha_fin;
 	}
 
-	public Date getFecha_evaluacion() {
-		return fecha_evaluacion;
-	}
 
-	public void setFecha_evaluacion(Date fecha_evaluacion) {
-		this.fecha_evaluacion = fecha_evaluacion;
-	}
-
-	public Date getFecha() {
+	public Date getFecha_publicacion() {
 		return fecha_publicacion;
 	}
 
-	public void setFecha(Date fecha) {
-		this.fecha_publicacion = fecha;
+	public void setFecha_publicacion(Date fecha_publicacion) {
+		this.fecha_publicacion = fecha_publicacion;
 	}
-	
+
+	public int getEvaluacion() {
+		return evaluacion;
+	}
+
+	public void setEvaluacion(int evaluacion) {
+		this.evaluacion = evaluacion;
+	}
+
+	public Date getFecha_sesion() {
+		return fecha_sesion;
+	}
+
+	public void setFecha_sesion(Date fecha_sesion) {
+		this.fecha_sesion = fecha_sesion;
+	}
+
+
 
 }
